a0d51970609023fe49b78d808abf7da3807ab00d
TaniaGJimenez <122074803+TaniaGJimenez@users.noreply.github.com>
2023-05-15 23:22:57 -0600

    Añade: Leer Directorio y Unificación de Codigo en MdLinks

--- api.js
+++ api.js
@@ -14,12 +14,51 @@ export const absPath = (pathUsed) => path.isAbsolute(pathUsed);
 export const transformPath = (converted) => path.resolve(converted);
 // console.log(transformPath('./Pruebas'), transformPath('ejemplo.md'));
 
-//TODO:
 //? La ruta es un directorio?
 export const isThisADirectory = (path) => fs.statSync(path).isDirectory();
 // console.log("La ruta es un directorio", isThisADirectory('./Pruebas'), isThisADirectory('ejemplo.md'));
 
-//TODO:
 //? Es un archivo .md
 export const isThisAMDFile = (pathUsed) => path.extname(pathUsed) === ".md";
 // console.log("Es un archivo .md", isThisAMDFile('./Pruebas'), isThisAMDFile('ejemplo.md'));
+
+//? Lee el directorio
+export const listDirectoryFiles = (directoryPath) => {
+    const files = [];
+  
+    const readDirectory = (dirPath) => {
+      const items = fs.readdirSync(dirPath);
+  
+      items.forEach((item) => {
+        const itemPath = path.join(dirPath, item);
+        const stat = fs.statSync(itemPath);
+  
+        if (stat.isDirectory()) {
+          readDirectory(itemPath); // Llamada recursiva si es un directorio
+        } else {
+          files.push(itemPath); // Agregar archivo a la lista
+        }
+      });
+    };
+  
+    readDirectory(directoryPath);
+  
+    return files;
+  };
+// console.log("Es un directorio con archivos", listDirectoryFiles('./Pruebas'));
+
+// export const getMarkdownLinks = (filePath) => {
+//     const links = [];
+//     const fileContent = fs.readFileSync(filePath, 'utf-8');
+//     const regex = /\[([^\]]+)\]\(([^\)]+)\)/g;
+  
+//     let match;
+//     while ((match = regex.exec(fileContent)) !== null) {
+//       const text = match[1];
+//       const url = match[2];
+//       links.push({ text, url });
+//     }
+  
+//     return links;
+//   };
+// console.log("Es un archivo .md", getMarkdownLinks('README_DataLovers.md'), getMarkdownLinks('ejemplo.md'));
--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-import { absPath, existingPath, transformPath, isThisADirectory, isThisAMDFile } from "./api.js";
+import { absPath, existingPath, transformPath, isThisADirectory, isThisAMDFile,listDirectoryFiles } from "./api.js";
 
-  export const mdLinks = (path, options) => {
-    return new Promise((resolve, reject) => {
-      const pathExists = existingPath(path);
-      if (pathExists) {
-        resolve(path);
-      } else {
-        reject(`La ruta ${path} no existe`);
-      }
-    }).then((path) => {
-      const isPathAbsolute = absPath(path);
-      const absolutePath = isPathAbsolute ? path : transformPath(path);
-      if (isThisADirectory(absolutePath)) {
-        console.log('La ruta es un directorio.',`Ruta:${absolutePath}`);
-      } else if (isThisAMDFile(absolutePath)) {
-        console.log('La ruta es un archivo .md',`Ruta:${absolutePath}`);
-      } else {
-        console.log('La ruta no es un archivo .md ni un directorio.');
-      }
-      return absolutePath;
-    }).catch((error) => {
-      console.error(error);
-    });
-  };
+export const mdLinks = (path, options) => {
+  return new Promise((resolve, reject) => {
+    const pathExists = existingPath(path);
+    if (pathExists) {
+      resolve(path);
+    } else {
+      reject(`La ruta ${path} no existe`);
+    }
+  }).then((path) => {
+    const isPathAbsolute = absPath(path);
+    const absolutePath = isPathAbsolute ? path : transformPath(path);
+    if (isThisADirectory(absolutePath)) {
+      console.log('La ruta es un directorio.', `Ruta:${absolutePath}`);
+      const files = listDirectoryFiles(absolutePath);
+      console.log('Archivos dentro del directorio:');
+      files.forEach((file) => console.log(file));
+    } else if (isThisAMDFile(absolutePath)) {
+      console.log('La ruta es un archivo .md', `Ruta:${absolutePath}`);
+    } else {
+      console.log('La ruta no es un archivo .md ni un directorio.');
+    }
+    return absolutePath;
+  }).catch((error) => {
+    console.error(error);
+  });
+};
   
 mdLinks('./Pruebas');
 mdLinks('ejemplo.md');
